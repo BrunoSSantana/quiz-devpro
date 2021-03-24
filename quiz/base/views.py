0912b948244d2b2cde5d172a83dd998b10502653
BrunoSSantana <brunoosouza15@gmail.com>
2021-03-24 17:59:32 -0300

    add migrations

--- quiz/base/views.py
+++ quiz/base/views.py
@@ -11,3 +11,7 @@ def home(request):
 def perguntas(request, indice):
     contexto = {'indice_da_questao': indice}
     return  render(request, 'base/game.html', context=contexto)
+
+
+def classificacao(request):
+    return render(request, 'base/classificacao.html')
