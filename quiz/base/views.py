6985b28940976d3afac5c4358d42058a5a83a8f2
BrunoSSantana <brunoosouza15@gmail.com>
2021-03-25 15:14:17 -0300

    verificacao das respostas e redirecionamento para classificacao

--- quiz/base/views.py
+++ quiz/base/views.py
@@ -23,13 +23,31 @@ def home(request):
                 return  render(request, 'base/home.html', contexto)
         else:
             request.session['aluno_id'] = aluno.id
+            return redirect('/perguntas/1')
     return  render(request, 'base/home.html')
 
 
 def perguntas(request, indice):
-    pergunta = Pergunta.objects.filter(disponivel=True).order_by('id')[indice - 1]
-    contexto = {'indice_da_questao': indice, 'pergunta': pergunta}
-    return  render(request, 'base/game.html', context=contexto)
+    try:
+        aluno_id = request.session['aluno_id']
+    except KeyError:
+        return redirect('/')
+    else:
+        try:
+            pergunta = Pergunta.objects.filter(disponivel=True).order_by('id')[indice - 1]
+        except IndexError:
+            return redirect('/classificacao')
+        else:
+            contexto = {'indice_da_questao': indice, 'pergunta': pergunta}
+
+            if request.method == 'POST':
+                resposta_indice = int(request.POST['resposta_indice'])
+                if resposta_indice == pergunta.alternativa_correta:
+                    # Armazenar dados da resposta
+                    return redirect(f'/perguntas/{indice + 1}')
+                contexto['resposta_indice'] = resposta_indice
+                    
+            return  render(request, 'base/game.html', context=contexto)
 
 
 def classificacao(request):
