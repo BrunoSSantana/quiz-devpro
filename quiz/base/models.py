e3f89565d9494b8c478035ee996e435a03929d3d
BrunoSSantana <brunoosouza15@gmail.com>
2021-03-25 19:01:43 -0300

    respostas entrando no db

--- quiz/base/models.py
+++ quiz/base/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+# from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
 
@@ -21,5 +22,16 @@ class Pergunta(models.Model):
     ])
 
 
-def __str__(self):
-    return self.enunciado
+    def __str__(self):
+        return self.enunciado
+
+class Resposta(models.Model):
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
+    pontos = models.IntegerField()
+    respondida_em = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['aluno', 'pergunta'], name='resposta_unica')
+        ]
